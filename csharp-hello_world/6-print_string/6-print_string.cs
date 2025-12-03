c574903e7ae38a8ead58b099e3fc90fb4594dc43
Anis Hamila <hlbn.hanis@gmail.com>
2025-12-03 12:04:53 +0100

    ?

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine("String: {0} {0} {0}", str);
+		Console.WriteLine("{0} {0} {0}", str);
         Console.WriteLine("{0}", str.Substring(0, 9));
 	}
 }
