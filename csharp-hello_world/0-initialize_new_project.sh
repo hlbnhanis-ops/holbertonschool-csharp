0ab71a9e3d7fdd0bcd1c07b6c1392f3b2666da80
Anis Hamila <hlbn.hanis@gmail.com>
2025-12-03 11:39:49 +0100

    ?

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
 dotnet new console -n 0-new_project
-
-cd 0-new_project
-
-mv Program.cs 0-new_project.cs
-sed -i 's|<Compile Include="Program.cs" />|<Compile Include="0-new_project.cs" />|' "0-new_project.csproj"
--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -4,7 +4,4 @@ dotnet new console -n 1-new_project
 
 cd 1-new_project
 
-mv Program.cs 1-new_project.cs
-sed -i 's|<Compile Include="Program.cs" />|<Compile Include="1-new_project.cs" />|' "1-new_project.csproj"
-
 dotnet build
