c25802b7559331f391234ddeb61f72a90e8f4308
Anis Hamila <hlbn.hanis@gmail.com>
2025-12-03 11:55:14 +0100

    ?

--- csharp-hello_world/3-writeline/3-writeline.cs
+++ csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("\"Programming is like building a multilingual puzzle");
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\"Programming is like building a multilingual puzzle");
+    }
+}
+
