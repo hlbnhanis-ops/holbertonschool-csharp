bba2fcc91cff35c03cd0e2abda4aa4637e7fec76
Anis Hamila <pro.anishamila@gmail.com>
2026-01-07 12:43:37 +0100

    Exceptions: safe_list_print

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _0_safe_list_print
+{
+    class List
+    {
+        public static int SafePrint(List<int> myList, int n)
+        {
+            int count = 0;
+
+            try
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(myList[i]);
+                    count++;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+            }
+
+            return count;
+        }
+}
+}
