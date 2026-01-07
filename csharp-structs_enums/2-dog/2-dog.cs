9d95d06e7b8f428d33033c275691b676bf122bff
Anis Hamila <pro.anishamila@gmail.com>
2026-01-07 14:46:47 +0100

    DOG3 1

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string aName, float anAge, string anOwner, Rating aRating)
+    {
+        name = aName;
+        age = anAge;
+        owner = anOwner;
+        rating = aRating;
+    }
+}
