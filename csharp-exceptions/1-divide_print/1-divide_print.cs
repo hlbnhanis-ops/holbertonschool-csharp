df9400aaff520d03c7101eb70b4e6bdcb2ebb457
Anis Hamila <pro.anishamila@gmail.com>
2026-01-07 12:51:41 +0100

    Exceptions : 1-divide_print 1

---
+++ csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Int{
+
+    public static void divide(int a, int b){
+        try{
+            int result = a / b;
+            Console.WriteLine("{0} / {1} = {2}", a, b, result);
+        }
+        catch{
+            Console.WriteLine("Cannot divide by zero");
+        }
+    }
+}
