400bc3564bf281917f2772b09d522570d3d20c19
Anis Hamila <pro.anishamila@gmail.com>
2026-01-13 14:42:02 +0100

    CLASSES 3 1

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Enemies
+{
+    public class Zombie
+    {
+        public int health;
+
+        public Zombie()
+        {
+            health = 0;
+        }
+
+        public Zombie(int anHealth)
+        {
+            if (anHealth < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            health = anHealth;
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
+
