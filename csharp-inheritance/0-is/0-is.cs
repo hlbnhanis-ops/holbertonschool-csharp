12b110fcaf1ca06b8168cf6f0b3eb2827cc68a09
Anis Hamila <pro.anishamila@gmail.com>
2026-01-26 03:44:41 +0100

    INHERITANCE 1 2

--- csharp-inheritance/0-is/0-is.cs
+++ csharp-inheritance/0-is/0-is.cs
@@ -2,6 +2,11 @@
 
 class Obj
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     public static bool IsOfTypeInt(object obj)
     {
         return obj is int;
--- csharp-inheritance/1-instance/1-instance.cs
+++ csharp-inheritance/1-instance/1-instance.cs
@@ -2,6 +2,11 @@
 
 class Obj
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <returns></returns>
     public static bool IsInstanceOfArray(object obj)
     {
         return obj is Array;
