6c3c1c7cf5054efb2c5de4624a89c6add211b3a0
Anis Hamila <pro.anishamila@gmail.com>
2026-01-26 21:06:13 +0100

    INHERITANCE 3 1

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -2,6 +2,12 @@
 
 class Obj
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="derivedType"></param>
+    /// <param name="baseType"></param>
+    /// <returns></returns>
     public static bool IsOnlyASubclass(Type derivedType, Type baseType)
     {
         return (derivedType != null && baseType != null && derivedType != baseType && baseType.IsAssignableFrom(derivedType));
---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Obj
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="myObj"></param>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine(String.Format("{0} Properties:", myObj.GetType().Name));
+        foreach (var i in myObj.GetType().GetProperties())
+        {
+            Console.WriteLine(i.ToString());
+        }
+        foreach (var i in myObj.GetType().GetMethods())
+        {
+            Console.WriteLine(i.ToString());
+        }
+    }
+}
