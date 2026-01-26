3c9f6f11ff22295cd9c397881e6e0caaf085d8da
Anis Hamila <pro.anishamila@gmail.com>
2026-01-26 09:11:08 +0100

    INHERITANCE 2 1

---
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Obj
+{
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        return (derivedType != baseType) && (derivedType is baseType);
+    }
+}
+
