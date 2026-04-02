278c6e7b1121e2aab8dfaa62eefd1cdd9d56f60b
Anis Hamila <pro.anishamila@gmail.com>
2026-04-02 13:20:00 +0200

    ?

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,58 @@
+﻿
+public class Node<T>
+{
+    public T value;
+    public Node<T> next;
+
+    public Node(T value)
+    {
+        this.value = T;
+        this.next = null;
+    }
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>
+{
+
+    public Node<T> Head{get;}
+    public Node<T> Tail{get;}
+    public int Count{get;}
+
+    public Queue()
+    {
+        Head = null;
+        Tail = null;
+        Count = 0;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString(); 
+    }
+
+    public void Enqueue(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+
+        if(Head == null)
+        {
+            Head = newNode;
+            Tail = newNode;
+        }
+        else
+        {
+            Tail.next = newNode;
+            Tail = newNode;
+        }
+
+        Count++;
+    }
+}
