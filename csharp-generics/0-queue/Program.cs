7f7eb8e08b72dac1a1c34cdb187f2207deb9feb3
Anis Hamila <pro.anishamila@gmail.com>
2026-04-02 12:50:15 +0200

    ?

---
+++ csharp-generics/0-queue/Program.cs
@@ -0,0 +1,15 @@
+﻿/// <summary>
+/// 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public string CheckType()
+    {
+        return typeof(T).ToString();
+    }
+}
