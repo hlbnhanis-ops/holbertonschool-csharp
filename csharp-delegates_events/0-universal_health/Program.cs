b44da8049b748499cf82ef6773d9f5c0e5ffb00b
Anis Hamila <pro.anishamila@gmail.com>
2026-04-06 07:18:33 +0200

    ?

---
+++ csharp-delegates_events/0-universal_health/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+
+        if(maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            this.hp = 100f;
+            Console.WriteLine("maxHp must be greater than 0.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
