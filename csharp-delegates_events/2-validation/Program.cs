bb2d631545538f8a16eae9e33712119f778fb5e1
Anis Hamila <pro.anishamila@gmail.com>
2026-04-06 07:34:33 +0200

    ?

---
+++ csharp-delegates_events/2-validation/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+    private delegate void CalculateHealth(float amount);
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+
+        if(maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            this.hp = 100f;
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+        }
+    }
+
+    public void PrintHealth()
+    {
+        System.Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(damage < 0) damage = 0;
+        System.Console.WriteLine($"{name} takes {damage} damage!");
+        ValidateHP(hp - damage);
+    }
+
+    public void HealDamage(float heal)
+    {
+        if(heal < 0) heal = 0;
+        System.Console.WriteLine($"{name} heals {heal} HP!");
+        ValidateHP(hp + heal);
+    }
+
+    public void ValidateHP(float newHp)
+    {
+        if(newHp < 0) hp = 0;
+        if(newHp > maxHp) hp = maxHp;
+        hp = newHp;
+    }
+}
